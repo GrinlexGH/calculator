8e4d642b364bc046505aa5bec3ccef2a4fbe387c
Grinlex <aaalllooollk@gmail.com>
2023-06-23 15:00:58 +0300

    get console width and height

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <sys/ioctl.h>
-#include <unistd.h>
+#if defined(_WIN32) || defined(_WIN64)
+	#include <Windows.h>
+#elif defined(__unix__)
+	#include <sys/ioctl.h>
+	#include <unistd.h>
 #endif
 
-void getConsoleSize(int& rows, int& cols) {
-#ifdef _WIN32
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-#else
-    struct winsize size;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-    cols = size.ws_col;
-    rows = size.ws_row;
+void getConsoleSize(int& columns, int& rows) {
+#if defined(_WIN32) || defined(_WIN64)
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+#elif defined(__unix__)
+	struct winsize size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+	columns = size.ws_col;
+	rows = size.ws_row;
 #endif
 }
 
 int main() {
-    int rows, cols;
-    getConsoleSize(rows, cols);
-    std::cout << "Console size: " << cols << " columns x " << rows << " rows" << std::endl;
-
+    int columns, rows;
+    getConsoleSize(columns, rows);
+    std::cout << "Console size: " << columns << " columns x " << rows << " rows" << std::endl;
     return 0;
 }
